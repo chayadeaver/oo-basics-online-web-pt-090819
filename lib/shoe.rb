f9a9e1553c0772b00da92100fc0a290dd4448f6a
Chaya Deaver <chayadeaver@gmail.com>
2019-09-18 00:58:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 # Make your shoe class here!
-  class Shoe 
-    
+  class Shoe
+
     def initialize(brand)
       @brand = brand
     end
-    
+
     def brand
       @brand
     end
     
-    
+
   end
